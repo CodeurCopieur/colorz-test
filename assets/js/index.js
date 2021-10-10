a5da767387c1d6f94e9cb2262fead9a47908f9b8
codwerk <louisws@outlook.fr>
2021-10-10 15:26:10 +0200

    use scrollTrigger (gsap)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   this.disabledScroll(true);
-  //this.sectionOneScrollAnimations();
-  //this.sectionTwoScrollAnimations();
+  this.sectionOneScrollAnimations();
+  this.sectionTwoScrollAnimations();
 
   this.initAnimations();
 });
@@ -37,17 +37,72 @@ function initAnimations() {
     duration: .8,
     y: 90,
     opacity: 0,
+    stagger: .15,
+    onComplete:() => this.disabledScroll(false)
+  })
+}
+
+function sectionOneScrollAnimations() {
+
+  const tl = gsap.timeline();
+
+  tl
+  .to( "#coverWrapperTexts > *", {
+    scrollTrigger: {
+      trigger: "#coverWrapperTexts",
+      scrub: true,
+      start: 'top top',
+      end: "center 20%"
+    },
+    duration: .6,
+    y: -40,
+    opacity: .3,
     stagger: .15
   })
+  .to( "#offreSpecial", {
+    scrollTrigger: {
+      trigger: "#cover",
+      scrub: true,
+      //    Trigger Scroller
+      start: 'top+=100 top+=100',
+      end: "center 20%"
+    },
+    y: 0,
+    autoAlpha: 1
+  })
+  .to("#introContainer > *", {
+    autoAlpha: 1,
+    stagger: .2,
+    delay: .2,
+    scrollTrigger: {
+      trigger: "#introContainer",
+      start: 'top-=200'
+    }
+  })
+}
 
+function sectionTwoScrollAnimations() {
+  const tl = gsap.timeline();
 
-  .from( "#offreSpecial", {
-    delay: .7,
-    duration: .2,
-    y: 70,
-    opacity: 0,
-    onComplete:() => this.disabledScroll(false)
-  });
+  tl
+  .from( "#advancedWrap > *", {
+    scrollTrigger: {
+      trigger: "#advanced",
+      scrub: true,
+    },
+    duration: .5,
+    x: -40,
+    opacity: .75
+  })
+  .from( "#advanced__wrapper-media", {
+    scrollTrigger: {
+      trigger: "#advanced",
+      scrub: true,
+    },
+    duration: .5,
+    x: 80,
+    opacity: .75
+  })
 }
 
 
